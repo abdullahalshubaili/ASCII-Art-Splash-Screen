8d895b55c346eb84a97e51b7cf8c34d22c5dbf2d
abdullahalshubaili <a.abunasser.1992@gmail.com>
2020-03-28 18:06:46 +0300

    fixing relative file and appending banner_path file in ~/

--- ascii.py
+++ ascii.py
@@ -1,20 +1,15 @@
 import random
 import os
-from pckgs import *
 import string
 
-#f = str(Getdir())
-#to strip "'" from the path 
-#a = f.strip("'")
 repath = "~/.banners_path"
 
 fpath = os.path.expanduser(repath)
 
 mypath = open(fpath, 'r')
 a = mypath.read()
-print(a)
 path = a + '/art/'
-print(path)
+
 choosing = random.choice(os.listdir(path))
 os.system("cat " + path + choosing)
 
